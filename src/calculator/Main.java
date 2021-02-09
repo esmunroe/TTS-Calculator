9ff94495162aaeffe3370d442dd7c76c33542132
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-09 09:47:48 -0500

    Use sample values to use calculator

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,8 +1,16 @@
 package calculator;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        MagicCalculator calc = new MagicCalculator();
+
+        final int firstNum = 1;
+        final int secondNum = 2;
+        final int magicNum = 5;
+
+        System.out.printf("Using '1' and '2' as values for the basic calculator...\nAdd: %d\nSubtract: %d\nMultiply: %d\nDivide: %d\n",
+                calc.add(firstNum, secondNum), calc.subtract(firstNum, secondNum), calc.multiply(firstNum, secondNum), calc.divide(firstNum, secondNum));
+        System.out.printf("Using '5' as a value for the magic calculator...\nSquare Root: %f\nSin: %f\nCosine: %f\nTangent: %f\nFactorial: %f\n",
+                calc.squareRoot(magicNum), calc.sin(magicNum), calc.cosine(magicNum), calc.tangent(magicNum), calc.factorial(magicNum));
     }
 }
