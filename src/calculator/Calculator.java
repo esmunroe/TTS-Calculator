9ddd5dafc72406fca78843609c148a55f245334f
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-09 09:17:58 -0500

    Calculator commands

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,23 @@
 package calculator;
 
 public class Calculator {
+    public int add(int firstNum, int secondNum){
+        return firstNum + secondNum;
+    }
+
+    public int subtract(int firstNum, int secondNum){
+        return firstNum - secondNum;
+    }
+
+    public int multiply(int firstNum, int secondNum){
+        return firstNum * secondNum;
+    }
+
+    public int divide(int firstNum, int secondNum){
+        return firstNum / secondNum;
+    }
+
+    public int square(int num){
+        return num * num;
+    }
 }
