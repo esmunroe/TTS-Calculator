1cfcc816ac7db699833deaed4d5523f6b0296266
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-09 09:22:11 -0500

    MagicCalculator method skeletons

--- src/calculator/MagicCalculator.java
+++ src/calculator/MagicCalculator.java
@@ -1,4 +1,23 @@
 package calculator;
 
 public class MagicCalculator extends Calculator{
+    public int squareRoot(int num){
+
+    }
+
+    public int sin(int num){
+
+    }
+
+    public int cosine(int num) {
+
+    }
+
+    public int tangent(int num){
+
+    }
+
+    public int factorial(int num){
+
+    }
 }
